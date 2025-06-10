0228e4938c6236391139ae03d3feee8d6664034c
gustavopatrocinio025 <gustavopatrocinio15@gmail.com>
2025-06-10 12:17:28 -0300

    alterações do dia 10/06

--- javascript/cep.js
+++ javascript/cep.js
@@ -1,29 +1,34 @@
 $(document).ready(function () {
-    $("#cep").on("blur", function () {
-        const cep = $(this).val().replace(/\D/g, "");
+  $("#cep").on("blur", function () {
+    //qdo a caixa perde o foco
+    // Verifica se o campo de CEP está vazio
+    const cep = $(this).val().replace(/\D/g, ""); // Remove caracteres não numéricos
 
-        if (cep.length === 8) {
-            $.getJSON(`https://viacep.com.br/ws/${cep}/json/`, function (data) {
-                if (!("erro" in data)) {
-                    $("#rua").val(data.logradouro);
-                    $("#bairro").val(data.bairro);
-                    $("#cidade").val(data.localidade);
-                    $("#estado").val(data.uf);
-                } else {
-                    alert("CEP não encontrado.");
-                    limparCampos();
-                }
-            }).fail(function () {
-                alert("Erro ao consultar o CEP.");
-                limparCampos();
-            });
+    // Verifica se o CEP tem 8 dígitos
+    // Se o CEP não for vazio e tiver 8 dígitos, faz a requisição
+    if (cep.length === 8) {
+      //https://viacep.com.br/ws/13304640/json/ para teste
+      $.getJSON(`https://viacep.com.br/ws/${cep}/json/`, function (data) {
+        if (!("erro" in data)) {
+          $("#rua").val(data.logradouro);
+          $("#bairro").val(data.bairro);
+          $("#cidade").val(data.localidade);
+          $("#estado").val(data.uf);
         } else {
-            alert("CEP inválido.");
-            limparCampos();
+          alert("CEP não encontrado.");
+          limparCampos();
         }
-    });
-
-    function limparCampos() {
-        $("#rua, #bairro, #cidade, #estado").val("");
+      }).fail(function () {
+        alert("Erro ao consultar o CEP.");
+        limparCampos();
+      });
+    } else {
+      alert("CEP inválido.");
+      limparCampos();
     }
+  });
+
+  function limparCampos() {
+    $("#rua, #bairro, #cidade, #estado").val("");
+  }
 });
